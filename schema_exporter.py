eea6f2f311c45dea9e3f5813b40eb4f00e3d842a
erdem.sari <erdem.sari@acibademsigorta.com.tr>
2019-11-21 16:26:51 +0300

    typo

--- schema_exporter.py
+++ schema_exporter.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python 
 import sys, cx_Oracle 
 
-db = cx_Oracle.connect('schema adı', 'password', 'tnsname')
+db = cx_Oracle.connect('schema name', 'password', 'tnsname')
 ca = db.cursor() 
-sch = ca.execute(ca.prepare("select username from all_users where username in ('schemaadı', 'OTHER_SCHEMA') order by username")).fetchall() 
+sch = ca.execute(ca.prepare("select username from all_users where username in ('schema name', 'OTHER_SCHEMA') order by username")).fetchall() 
 q = ca.prepare('select fetch_schema_metadata(:1, \'%\') as text from dual') 
 for schema in [x[0] for x in sch]: 
      c = db.cursor() 
