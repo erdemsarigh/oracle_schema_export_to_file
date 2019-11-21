9735627529f24aaf4290e56582a6dd8b48f4ab72
erdem.sari <erdem.sari@acibademsigorta.com.tr>
2019-11-21 17:14:03 +0300

    run opti

--- schema_exporter.py
+++ schema_exporter.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python 
-import sys, cx_Oracle 
+import sys, cx_Oracle, argparse
 
-db = cx_Oracle.connect('schema name', 'password', 'tnsname')
+
+# parser = argparse.ArgumentParser(description='Export Oracle object as text.')
+# parser.add_argument('schema', dest='schema', help='schema name')
+# parser.add_argument('password',dest='password', help='schema password')
+# parser.add_argument('TNS',dest='TNS', help='TNS name')
+
+# args = parser.parse_args()
+# print args.schema
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-s', action='store', dest='schema',  help='Schema name')
+parser.add_argument('-p', action='store', dest='password',  help='Schema password')
+parser.add_argument('-t', action='store', dest='tns',  help='TNS name')
+
+args = parser.parse_args()
+
+
+db = cx_Oracle.connect(args.schema,args.password,args.tns)
+
 ca = db.cursor() 
-sch = ca.execute(ca.prepare("select username from all_users where username in ('schema name', 'OTHER_SCHEMA') order by username")).fetchall() 
-q = ca.prepare('select fetch_schema_metadata(:1, \'%\') as text from dual') 
-for schema in [x[0] for x in sch]: 
+sch = ca.execute(ca.prepare("select object_name from all_objects where lower(owner) = lower('" + args.schema  + "')  and lower(object_name) like lower('PKG_B%')")).fetchall() 
+for objectname in [x[0] for x in sch]: 
+     q = ca.prepare('select fetch_schema_metadata(:1, :2) as text from dual') 
      c = db.cursor() 
-     res = ca.execute(q, [schema.upper(), ]) 
-     f = open('%s.sql' % (schema), 'w') 
+     res = ca.execute(q, [ args.schema,  objectname.upper(),]) 
+     f = open('%s.sql' % (objectname), 'w') 
      f.write(res.fetchall()[0][0].read())
      f.close()
      c.close()
